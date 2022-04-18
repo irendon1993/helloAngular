5e2b8980f6e27313b3b12e39c7321d2079f61d77
irendon1993 <isaacrendon7@gmail.com>
2022-04-18 12:20:17 -0400

    Created two custom components for Assignemnt 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.componenet';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/success/success.componenet.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'success',
+  templateUrl: 'success.component.html'
+
+})
+export class SuccessComponent {
+  
+}
