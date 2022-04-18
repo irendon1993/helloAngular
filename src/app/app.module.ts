a4ec1c5449d00af553b8d194969e42a5b76f1511
irendon1993 <isaacrendon7@gmail.com>
2022-04-18 13:36:04 -0400

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -14,10 +13,12 @@ import { SuccessComponent } from './success/success.componenet';
     ServerComponent,
     ServersComponent,
     WarningComponent,
-    SuccessComponent
+    SuccessComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
