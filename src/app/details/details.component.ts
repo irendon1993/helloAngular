00b51f426b57ae481b663d83f0b38ad4f5b9f81d
irendon1993 <isaacrendon7@gmail.com>
2022-04-25 15:13:28 -0400

    Finsihed assingment 3

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,19 +2,27 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-details',
-  templateUrl: './details.component.html'
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
 })
 
 export class DetailsComponent {
   displayInfo = false;
+  logArray = [];
 
   displayDetails() {
-    if(this.displayInfo === false) {
-      this.displayInfo = true;
-    }
-    else {
-      this.displayInfo = false;
-    }
+    // if(this.displayInfo === false) {
+    //   this.displayInfo = true;
+    //   this.logArray.push(this.displayInfo)
+    //   console.log(this.logArray)
+    // }
+    // else {
+    //   this.displayInfo = false;
+    //   this.logArray.push(this.displayInfo)
+    //   console.log(this.logArray)
+    // }
+    this.displayInfo= !this.displayInfo;
+    this.logArray.push(this.logArray.length + 1)
    
   }
 }
